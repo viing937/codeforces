fbe43285f8e5b4961674871696c723d94a2b3163
ving <viing937@gmail.com>
2015-05-01 10:30:05 +0800

    Fri May  1 10:30:05 CST 2015

--- src/253A.rb
+++ src/253A.rb
@@ -1,10 +1,11 @@
-file = File.new("input.txt", "r")
-n, m = file.gets.split.collect{|i| i.to_i}
-file.close
-file = File.new("output.txt", "w")
-if n >= m
-    file.puts (['B','G']*m+['B']*(n-m)).join
-else
-    file.puts (['G','B']*n+['G']*(m-n)).join
+n, m = 0, 0
+File.open("input.txt", "r") do |file|
+    n, m = file.gets.split.collect{|i| i.to_i}
 end
-file.close
+File.open("output.txt", "w") do |file|
+    if n >= m
+        file.puts (['B','G']*m+['B']*(n-m)).join
+    else
+        file.puts (['G','B']*n+['G']*(m-n)).join
+    end
+end
