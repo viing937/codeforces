721a728e2af07e338075c85d057308b31883f664
ving <viing937@gmail.com>
2015-05-06 10:53:28 +0800

    Wed May  6 10:53:28 CST 2015

---
+++ src/534B.rb
@@ -0,0 +1,14 @@
+v1, v2 = gets.split.collect{|i| i.to_i}.minmax
+t, d = gets.split.collect{|i| i.to_i}
+if d.zero?
+    puts v1*t
+    exit
+end
+x = (1.0*d*t+v2-v1)/(2*d)
+if x%1 >= 0.5
+    x = x.ceil
+else
+    x = x.floor
+end
+ans = v1*x+d*(x-1)*x/2 + v2*(t-x)+d*(t-x-1)*(t-x)/2
+puts ans
