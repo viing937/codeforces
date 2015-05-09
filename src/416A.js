c40e59bf02f74b55299520c48011fd7224f30ec1
ving <viing937@gmail.com>
2015-05-09 11:21:53 +0800

    Sat May  9 11:21:53 CST 2015

---
+++ src/416A.js
@@ -0,0 +1,56 @@
+function main()
+{
+    var n = parseInt(readline());
+    var up = 1<<30;
+    var low = -up;
+    while ( n-- )
+    {
+        var tmp = readline().split(" ");
+        tmp[1] = parseInt(tmp[1]);
+        if ( tmp[2] == "N" )
+        {
+            if ( tmp[0] == ">=" )
+            {
+                tmp[0] = "<";
+            }
+            else if ( tmp[0] == "<=" )
+            {
+                tmp[0] = ">";
+            }
+            else if ( tmp[0] == ">" )
+            {
+                tmp[0] = "<=";
+            }
+            else if ( tmp[0] == "<" )
+            {
+                tmp[0] = ">=";
+            }
+        }
+        if ( tmp[0] == ">" && tmp[1]+1 > low )
+        {
+                low = tmp[1]+1;
+        }
+        else if ( tmp[0] == ">=" && tmp[1] > low )
+        {
+                low = tmp[1];
+        }
+        else if ( tmp[0] == "<" && tmp[1]-1 < up )
+        {
+                up = tmp[1]-1;
+        }
+        else if ( tmp[0] == "<=" && tmp[1] < up )
+        {
+                up = tmp[1];
+        }
+    }
+    if ( up < low )
+    {
+        print("Impossible");
+    }
+    else
+    {
+        print(low);
+    }
+}
+
+main();
