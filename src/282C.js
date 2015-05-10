0fa656f7bd4ce448fbc10570f1e505222331c3ac
ving <viing937@gmail.com>
2015-05-10 09:56:02 +0800

    Sun May 10 09:56:02 CST 2015

---
+++ src/282C.js
@@ -0,0 +1,25 @@
+function main()
+{
+    var a = readline(),
+        b = readline();
+    if ( a.length != b.length )
+    {
+        print("NO");
+        return;
+    }
+    if ( a == b )
+    {
+        print("YES");
+        return;
+    }
+    if ( a.match(/1/) && b.match(/1/) )
+    {
+        print("YES");
+    }
+    else
+    {
+        print("NO");
+    }
+}
+
+main();
