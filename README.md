b2d3e19bbdded08626482ee2280a75a5b7ac40bf
ving <viing937@gmail.com>
2015-06-29 10:34:43 +0800

    Mon Jun 29 10:34:43 CST 2015

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Example:
     sender = email_address
     password = login_password
     target = kindle@kindle.com
+    proxy = 127.0.0.1
+    proxy_port = 1080
+
+## notice
+
+In order to use gmail to send emails, enable ["Access for less secure apps"](https://www.google.com/settings/security/lesssecureapps), this allows you to use the google smtp for clients other than the official ones.
--- send_to_kindle.py
+++ send_to_kindle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import os
 import smtplib
+import socks
 
 def load():
     config = {}
     with open(os.environ["HOME"]+"/.send_to_kindle", "r") as f:
         for line in f.readlines():
-            line = line.rstrip("\n").split("=")
+            line = line.rstrip("\n").split("=", 1)
             config[line[0].strip(" ")] = line[1].strip(" ")
     return config
 
@@ -23,9 +24,15 @@ def send(config):
         print("Error sending mail.")
     server.quit()
 
+def setproxy(config):
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, config["proxy"], int(config["proxy_port"]))
+    socks.wrapmodule(smtplib)
+    return socks
+
 def main():
     config = load()
-    send(config)
+    if "proxy" in config.keys():
+        socks = setproxy(config)
 
 if __name__ == '__main__':
     main()
