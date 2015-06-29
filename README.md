f17caa9d2fed4b815fd1c43cf99117af44b59488
ving <viing937@gmail.com>
2015-06-29 10:02:00 +0800

    Mon Jun 29 10:02:00 CST 2015

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # send_to_kindle
+
+Send books to kindle via email.
+
+## config
+
+Create a config file ~/.send_to_kindle.
+Example:
+
+    server = smtp.qq.com
+    port = 465
+    sender = email_address
+    password = login_password
+    target = kindle@kindle.com
--- send_to_kindle.py
+++ send_to_kindle.py
@@ -2,11 +2,30 @@
 import os
 import smtplib
 
-config = {}
-with open(os.environ["HOME"]+"/.send_to_kindle", "r") as f:
-    for line in f.readlines():
-        line = line.rstrip("\n").split("=")
-        config[line[0].strip(" ")] = line[1].strip(" ")
+def load():
+    config = {}
+    with open(os.environ["HOME"]+"/.send_to_kindle", "r") as f:
+        for line in f.readlines():
+            line = line.rstrip("\n").split("=")
+            config[line[0].strip(" ")] = line[1].strip(" ")
+    return config
 
-print(config)
+def send(config):
+    server = smtplib.SMTP_SSL(config["server"], int(config["port"]))
+    server.login(config["sender"], config["password"])
+    body = '\r\n'.join(["To: %s" % config["target"],
+                        "From: %s" % config["sender"],
+                        "Subject: TEST"])
+    try:
+        server.sendmail(config["sender"], config["target"], body)
+        print("Email sent.")
+    except:
+        print("Error sending mail.")
+    server.quit()
 
+def main():
+    config = load()
+    send(config)
+
+if __name__ == '__main__':
+    main()
