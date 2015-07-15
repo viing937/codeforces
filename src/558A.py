1a2438281378cc163dcba17850b0cad1ecb97c3a
ving <viing937@gmail.com>
2015-07-15 08:50:55 +0800

    Wed Jul 15 08:50:54 CST 2015

--- src/558A.py
+++ src/558A.py
@@ -7,23 +7,15 @@ for i in range(n):
         left.append([x, a])
     else:
         right.append([x, a])
-left.sort()
+left.sort(reverse=True)
 right.sort()
 ans = 0
 length = min(len(left), len(right))
-for i in left[::-1]:
-    if length <= 0:
-        break
-    ans += i[1]
-    length -= 1
-length = min(len(left), len(right))
-for i in right:
-    if length <= 0:
-        break
-    ans += i[1]
-    length -= 1
+for i in range(length):
+    ans += left[i][1]
+    ans += right[i][1]
 if len(left) > len(right):
-    ans += left[::-1][len(right)][1]
+    ans += left[length][1]
 elif len(right) > len(left):
-    ans += right[len(left)][1]
+    ans += right[length][1]
 print(ans)
