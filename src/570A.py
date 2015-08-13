a4c539deb0bbf85535947674caa1bf4e138d54d5
ving <viing937@gmail.com>
2015-08-14 04:52:29 +0800

    Fri Aug 14 04:52:29 CST 2015

--- src/570A.py
+++ src/570A.py
@@ -2,7 +2,5 @@ n, m = [int(i) for i in input().split()]
 cnt = list([0]*(n+1))
 for i in range(m):
     votes = [int(num) for num in input().split()]
-    maxnum = max(votes)
-    cnt[votes.index(maxnum)] += 1
-maxnum = max(cnt)
-print(cnt.index(maxnum)+1)
+    cnt[votes.index(max(votes))] += 1
+print(cnt.index(max(cnt))+1)
