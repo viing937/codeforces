0ede8ccd8fad59ba654de8b34e88814f8b536779
ving <viing937@gmail.com>
2015-11-03 17:06:30 +0800

    Tue Nov  3 17:06:30 CST 2015

--- other/clock.cpp
+++ other/clock.cpp
@@ -1,24 +1,47 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
-#include <time.h>
+#include <cstdio>
+#include <ctime>
+#include <algorithm>
+using namespace std;
 
-#define SIZE 320
-#define HOURHAND 60 
-#define MINUHAND 100
-#define SECOHAND 120
+#define SIZE 640
+#define HOURHAND 120
+#define MINUHAND 220
+#define SECOHAND 240
+#define AAbits 8
 
-void draw_circle(SDL_Renderer *renderer, int x, int y, int r)
+int draw_point_RGBA(SDL_Renderer *renderer, int x, int y, int r, int g, int b, int a)
+{
+    int result = 0;
+    result |= SDL_SetRenderDrawBlendMode(renderer, (a == 255) ? SDL_BLENDMODE_NONE : SDL_BLENDMODE_BLEND);
+    result |= SDL_SetRenderDrawColor(renderer, r, g, b, a);
+    result |= SDL_RenderDrawPoint(renderer, x, y);
+    return result;
+}
+
+int draw_point_RGBA_weight(SDL_Renderer *renderer, int x, int y, int r, int g, int b, int a, int weight)
+{
+    int ax = (a*weight)>>8;
+    if ( ax > 255 )
+        a = 255;
+    else
+        a = ax & 0x000000ff;
+    return draw_point_RGBA(renderer, x, y, r, g, b, a);
+}
+
+int draw_circle(SDL_Renderer *renderer, int x, int y, int radius, int r, int g, int b, int a)
 {
     double deg_0 = 0;
     double deg_90 = M_PI/2;
-    double deg_step = deg_90 / (r*4);
-    while ( r-- )
+    double deg_step = deg_90 / (radius*4);
+    SDL_SetRenderDrawColor(renderer, r, g, b, a);
+    while ( radius-- )
     {
         double curr_deg = deg_0;
         while ( curr_deg < deg_90 )
         {
-            int offset_x = cos(curr_deg)*r,
-                offset_y = sin(curr_deg)*r;
+            int offset_x = cos(curr_deg)*radius,
+                offset_y = sin(curr_deg)*radius;
             SDL_RenderDrawPoint(renderer, x+offset_x, y+offset_y);
             SDL_RenderDrawPoint(renderer, x+offset_x, y-offset_y);
             SDL_RenderDrawPoint(renderer, x-offset_x, y+offset_y);
@@ -26,22 +49,92 @@ void draw_circle(SDL_Renderer *renderer, int x, int y, int r)
             curr_deg += deg_step;
         }
     }
-    return;
+    return 0;
 }
 
-void draw_hands(SDL_Renderer *renderer, struct tm *timer)
+int draw_smooth_line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int r, int g, int b, int a)
+{
+    if ( y1 > y2 )
+    {
+        swap(x1, x2);
+        swap(y1, y2);
+    }
+
+    int dx = x2-x1, dy = y2-y1;
+
+    int xdir;
+    if ( dx >= 0 )
+        xdir = 1;
+    else
+        xdir = -1, dx = -dx;
+
+    if ( dx == 0 || dy == 0 || dx == dy )
+    {
+        SDL_SetRenderDrawColor(renderer, r, g, b, a);
+        return SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+    }
+
+    int result = 0, erracc = 0;
+    int intshift = 32 - AAbits;
+
+    result |= draw_point_RGBA(renderer, x1, y1, r, g, b, a);
+    if ( dy > dx )
+    {
+        int erradj = ((dx<<16) / dy) << 16;
+        int x1pxdir = x1 + xdir;
+        while ( --dy )
+        {
+            int erracctmp = erracc;
+            erracc += erradj;
+            if ( erracc <= erracctmp )
+            {
+                x1 = x1pxdir;
+                x1pxdir += xdir;
+            }
+            y1 += 1;
+
+            int wgt = (erracc>>intshift) & 255;
+            result |= draw_point_RGBA_weight(renderer, x1, y1, r, g, b, a, 255-wgt);
+            result |= draw_point_RGBA_weight(renderer, x1pxdir, y1, r, g, b, a, wgt);
+        }
+    }
+    else
+    {
+        int erradj = ((dy<<16) / dx) << 16;
+        int y1pxdir = y1 + 1;
+        while ( --dx )
+        {
+            int erracctmp = erracc;
+            erracc += erradj;
+            if ( erracc <= erracctmp )
+            {
+                y1 = y1pxdir;
+                y1pxdir += 1;
+            }
+            x1 += xdir;
+
+            int wgt = (erracc>>intshift) & 255;
+            result |= draw_point_RGBA_weight(renderer, x1, y1, r, g, b, a, 255-wgt);
+            result |= draw_point_RGBA_weight(renderer, x1, y1pxdir, r, g, b, a, wgt);
+        }
+    }
+
+    result |= draw_point_RGBA(renderer, x2, y2, r, g, b, a);
+
+    return result;
+}
+
+int draw_hands(SDL_Renderer *renderer, struct tm *timer)
 {
     int x = SIZE/2, y = SIZE/2;
     double deg_sec = (double)(timer->tm_sec)/60*M_PI*2;
-    double deg_min = (double)(timer->tm_min)/60*M_PI*2+deg_sec/12;
+    double deg_min = (double)(timer->tm_min)/60*M_PI*2;
     double deg_hour = (double)(timer->tm_hour%12)/12*M_PI*2+deg_min/12;
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderDrawLine(renderer, x, y, x+sin(deg_sec)*SECOHAND, y-cos(deg_sec)*SECOHAND);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderDrawLine(renderer, x, y, x+sin(deg_min)*MINUHAND, y-cos(deg_min)*MINUHAND);
-    SDL_RenderDrawLine(renderer, x, y, x+sin(deg_hour)*HOURHAND, y-cos(deg_hour)*HOURHAND);
-    return;
+    draw_smooth_line(renderer, x, y, x+sin(deg_sec)*SECOHAND, y-cos(deg_sec)*SECOHAND, 255, 0, 0, 255);
+    draw_smooth_line(renderer, x, y, x+sin(deg_min)*MINUHAND, y-cos(deg_min)*MINUHAND, 0, 0, 0, 255);
+    draw_smooth_line(renderer, x, y, x+sin(deg_hour)*HOURHAND, y-cos(deg_hour)*HOURHAND, 0, 0, 0, 255);
+    return 0;
 }
 
 void update(SDL_Renderer *renderer)
@@ -50,11 +143,16 @@ void update(SDL_Renderer *renderer)
     time_t rawtime = time(NULL);
     struct tm *timer = localtime(&rawtime);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    draw_circle(renderer, SIZE/2, SIZE/2, SIZE/2*0.95);
+    draw_circle(renderer, SIZE/2, SIZE/2, SIZE/2*0.95, 255, 255, 255, 255);
+    for ( int i = 0; i < 12; ++i )
+    {
+        double deg = (double)i/12*M_PI*2;
+        draw_smooth_line(renderer, SIZE/2+sin(deg)*((SIZE/2)*0.95-10), SIZE/2-cos(deg)*((SIZE/2)*0.95-10),
+                                   SIZE/2+sin(deg)*(SIZE/2)*0.95, SIZE/2-cos(deg)*((SIZE/2)*0.95),
+                                   0, 0, 0, 255);
+    }
     draw_hands(renderer, timer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    draw_circle(renderer, SIZE/2, SIZE/2, SIZE/2*0.03);
+    draw_circle(renderer, SIZE/2, SIZE/2, SIZE/2*0.03, 0, 0, 0, 255);
     
     SDL_RenderPresent(renderer);
     return;
