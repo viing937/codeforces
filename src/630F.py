3e962272e68f6d9accf573abbbc69f278345eb7d
ving <viing937@gmail.com>
2016-02-22 13:20:27 +0800

    630F

--- src/630F.py
+++ src/630F.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
 def c(n, m):
-    return 0 if n>m else reduce(lambda a,b: a*b, range(m-n+1, m+1))//reduce(lambda a,b: a*b, range(1,n+1))
+    return 0 if n>m or n<0 else reduce(lambda a,b: a*b, range(m-n+1, m+1))//reduce(lambda a,b: a*b, range(1,n+1))
 
 n = int(input())
 print(sum([c(i, n) for i in range(5, 8)]))
