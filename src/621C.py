2dd0301c082e0c1271a7f5cdbbcf2ae6ff29456e
ving <viing937@gmail.com>
2016-03-02 17:55:50 +0800

    621C

--- src/621C.py
+++ src/621C.py
@@ -1,12 +1,7 @@
 n, p = map(int, input().split(' '))
 
-s = [0]*n
-for i in range(n):
-    l, r = map(int, input().split(' '))
-    s[i] = (r//p*p-(l-1)//p*p)/p/(r-l+1)
-
-ans = 0
-for i in range(n):
-    ans += (1-(1-s[i])*(1-s[i-1]))*2000
+lines = [list(map(int, input().split(' '))) for i in range(n)]
+porb = [(r//p-(l-1)//p)/(r-l+1) for l, r in lines]
+ans = sum([(1-(1-porb[i])*(1-porb[i-1])) for i in range(n)])*2000
 
 print(ans)
