592d017350b77336b6db54bddaeba033a2356e7e
Ving <viing937@gmail.com>
2017-04-23 17:13:37 +0800

    update login suda gateway script

--- other/login_gateway.py
+++ other/login_gateway.py
@@ -13,13 +13,14 @@ action = '登陆网关'
 r = requests.get('http://wg.suda.edu.cn')
 
 # __VIEWSTATE和__EVENTVALIDATION
-soup = BeautifulSoup(r.content)
+soup = BeautifulSoup(r.content, "html.parser")
 viewstate = soup.find_all('input')[0]['value']
-eventvalidation = soup.find_all('input')[13]['value']
+eventvalidation = soup.find_all('input')[1]['value']
 
 # params 
 params = {}
 params['__VIEWSTATE'] = viewstate
+params['__EVENTVALIDATION'] = eventvalidation
 params['TextBox1'] = user 
 params['TextBox2'] = passwd
 params['nw'] = 'RadioButton1'
@@ -28,11 +29,10 @@ if action == '登陆网关':
     params['Button1'] = action
 elif action == '退出网关':
     params['Button4'] = action
-params['__EVENTVALIDATION'] = eventvalidation
 
 # login 
 r = requests.post('http://wg.suda.edu.cn', data=params)
 
 # reponse 
-soup = BeautifulSoup(r.content)
-print soup.find_all('li')[3].get_text().strip()
+soup = BeautifulSoup(r.content, "html.parser")
+print(soup.find_all('li')[3].get_text().strip())
