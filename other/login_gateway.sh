534e05aff5c86d3ac244806c1fe500d6d8db9169
Ving <viing937@gmail.com>
2017-04-23 18:52:56 +0800

    migrate login_gateway to bash script from python

---
+++ other/login_gateway.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+VIEWSTATE=`curl wg.suda.edu.cn 2>/dev/null | grep __VIEWSTATE | sed 's/\(.*\)value="\(.*\)"\(.*\)/\2/g'`
+EVENTVALIDATION=`curl wg.suda.edu.cn 2>/dev/null | grep __EVENTVALIDATION | sed 's/\(.*\)value="\(.*\)"\(.*\)/\2/g'`
+user=$1
+passwd=$2
+tm='RadioButton8' # 10 hours
+
+curl wg.suda.edu.cn --data "__VIEWSTATE=$VIEWSTATE&__EVENTVALIDATION=$EVENTVALIDATION&TextBox1=$user&TextBox2=$passwd&nw=RadioButton1&tm=$tm&Button1=登陆网关" 2>/dev/null \
+                    | grep $user \
+                    | tr -d ' \t' \
+                    | sed 's/<\(.*\)>//g'
