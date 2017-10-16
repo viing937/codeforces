ebdf60aa1bbf09a0d5a2d50188cba3dadbe4a7d9
Ving <viing937@gmail.com>
2017-10-16 10:46:03 +0800

    761A

---
+++ src/761A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (abs(a-b) > 1 || (a == 0 && b == 0))
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
+    return 0;
+}
