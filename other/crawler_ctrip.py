d1a610315f1d1227edfd477a2401e09f39859321
Ving <viing937@gmail.com>
2017-12-11 19:17:41 +0800

    crawler - more urls

--- other/crawler_ctrip.py
+++ other/crawler_ctrip.py
@@ -2,18 +2,19 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-import re
+import random
 
 urlsFile = 'urls.txt'
 visitedUrlsFile = 'visited.txt'
 dataDir = 'data/'
+domain = 'http://you.ctrip.com'
 
-def get_url(urls, bs):
+def get_relative_url(urls, bs):
     for link in bs.find_all(attrs = {'class': 'asklink'}):
         url = link.get('href')
         urls.add(url)
-        with open(urlsFile, 'w') as f:
-            f.write('\n'.join(urls))
+    with open(urlsFile, 'w') as f:
+        f.write('\n'.join(urls))
 
 def get_info(bs, url):
     try:
@@ -27,8 +28,21 @@ def get_info(bs, url):
         with open(visitedUrlsFile, 'a') as f:
             f.write(url+'\n')
         print(title)
-    except:
-        pass
+    except Exception as err:
+        print(err)
+
+def get_url(urls):
+    url = '/asks/p' + str(random.randint(1, 50))
+    r = requests.get(domain+url, headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'})
+    bs = BeautifulSoup(r.text, 'html.parser')
+    asklist = bs.find(attrs = {'class': 'asklist'})
+    for ask in asklist.children:
+        try:
+            urls.add(ask.get('href'))
+        except Exception as err:
+            pass
+    with open(urlsFile, 'w') as f:
+        f.write('\n'.join(urls))
 
 def load_urls():
     with open(urlsFile, 'r') as f:
@@ -39,18 +53,20 @@ def load_visited():
         return set([url for url in f.read().split('\n') if url != ''])
 
 def main():
-    domain = 'http://you.ctrip.com'
     urls = load_urls()
+    get_url(urls)
     visited = load_visited()
     while len(urls) > 0:
         url = urls.pop()
-        while url in visited:
+        while len(urls) > 0 and url in visited:
             url = urls.pop()
+        if len(urls) == 0:
+            get_url(urls)
         visited.add(url)
         r = requests.get(domain+url, headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0'})
-        bs = BeautifulSoup(r.text, 'html5lib')
+        bs = BeautifulSoup(r.text, 'html.parser')
         get_info(bs, url)
-        get_url(urls, bs)
+        get_relative_url(urls, bs)
         time.sleep(5)
 
 if __name__ == '__main__':
