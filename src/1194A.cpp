8128c15a1ac2cde2fd0c2fca073b3f47fde33aa8
Ving <viing937@gmail.com>
2019-07-15 20:27:47 +0800

    1194A

---
+++ src/1194A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int n, x;
+        cin >> n >> x;
+        cout << x * 2 << endl;
+    }
+    return 0;
+}
