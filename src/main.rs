e5b2a6b487722d6b15b08c72a73fad6aa919326b
Ving <viing937@gmail.com>
2020-05-11 14:26:43 +0800

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::Write;
+
 fn main() {
-    println!("hello world");
+    let mut stdin = BufReader::new(io::stdin());
+    let mut stdout = BufWriter::new(io::stdout());
+    let mut buffer = String::new();
+
+    stdin.read_line(&mut buffer).unwrap();
+    let t: i32 = buffer.trim().parse().unwrap();
+    writeln!(stdout, "{}", t).unwrap();
 }
